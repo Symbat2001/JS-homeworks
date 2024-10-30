ffc5ba5ce9799838bf13390dd632c71db397c09b
Symbat <tazhben01@gmail.com>
2024-10-31 01:09:14 +0500

    консольге кир

---
+++ w-9, d-3, homework/js/script.js
@@ -0,0 +1,14 @@
+let name = "Абзал";
+let age = 23;
+let isStudent = true;
+
+console.log("Аты: " + name + " | Тип: " + typeof name);
+console.log("Жасы: " + age + " | Тип: " + typeof age);
+console.log("Студент пе?: " + isStudent + " | Тип: " + typeof isStudent);
+
+
+
+age = age.toString();
+console.log("Жасы (жол ретинде): " + age + " | Тип: " + typeof age);
+
+
