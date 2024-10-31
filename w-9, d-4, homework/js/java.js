d61dd955cde1ff0592df2a7d35e05653cd1e1adc
Symbat <tazhben01@gmail.com>
2024-11-01 01:43:30 +0500

    Консольге кир

---
+++ w-9, d-4, homework/js/java.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt("биринши санды енгиз"));
+let num2 = Number(prompt("екинши санды енгиз"));
+let num3 = Number(prompt("ушинши санды енгиз"));
+
+let average = (num1 + num2 + num3 ) / 3;
+
+alert("Арифметикалык ортасын тап: " + average); 
