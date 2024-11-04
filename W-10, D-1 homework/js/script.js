6756a5f05550af708029e70472360b5741988854
Symbat <tazhben01@gmail.com>
2024-11-05 00:19:42 +0500

    homework day 1

---
+++ W-10, D-1 homework/js/script.js
@@ -0,0 +1,27 @@
+// 1 exercise //
+
+// let age = 70;
+// let isStudent = false;
+
+// alert(age>65 || isStudent);
+let age = Number(prompt("Жасыныз каншада?"));
+let isStudent = Number(prompt("Сиз студентсизбе? 1-ия, 2-жок"));
+
+alert(age>65 || isStudent==1);
+
+
+// 2 тапсырма //
+
+let num = Number(prompt("Санды енгизиниз"));
+alert(num>=10 && num<=20);
+
+// 3 тапсырма //
+
+let num1 = Number(prompt("1 санды енгизиниз"));
+let num2 = Number(prompt("2 санды енгизиниз"));
+
+alert(a<0 && b>0 || a>0 && b<0);
+
+
+
+
