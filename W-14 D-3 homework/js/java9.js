96d8ea1a3d06a147ec50c3f9b2db80cc934b3c03
Symbat <tazhben01@gmail.com>
2024-12-05 13:58:05 +0500

    Homework

---
+++ W-14 D-3 homework/js/java9.js
@@ -0,0 +1,42 @@
+let classA = ["Амина","Адил","Алина"]
+let classB = ["Берик","Бакыт","Баян"]
+let classC = ["Амина","Адил","Алина","Берик","Бакыт","Баян"]
+
+classC.unshift("Директор");
+classC.push("Мугалим");
+console.log(classC);
+
+
+// 1 tapsyrma
+
+let students = [
+  {
+    name: "Ali",
+    age: 17,
+    grades: {
+        math: 85,
+        english: 90,
+        science: 80,
+    },
+    address: {
+        city: "Almaty",
+        street: "Abaya",
+    }
+   },
+   {
+    name: "Omar",
+    age: 22,
+    grades: {
+        math: 80,
+        english: 100,
+        science: 94,
+    },
+    address: {
+        city: "Astana",
+        street: "Tauelsizdik"
+    }
+   }   
+]
+let {name,grades: {math}, address: {city}, country = "Qazaqstan"} = students[0];
+
+console.log(`Name: ${name}`);
