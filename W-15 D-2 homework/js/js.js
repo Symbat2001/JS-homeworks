736022de100cb7ed3fc1303740455c28f9805fa3
Symbat <tazhben01@gmail.com>
2024-12-11 01:01:41 +0500

    Homework to DOM

---
+++ W-15 D-2 homework/js/js.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").textContent="HW: Introduction to DOM";
+
+document.getElementById("username").textContent="Tazhben Symbat";
+
+let barlygy = document.getElementsByClassName("hobbylar")
+
+barlygy[0].innerHTML= "<b>Drawing</b>"
+barlygy[1].innerHTML= "<b>Coding</b>"
+barlygy[2].innerHTML= "<b>Playing</b>"
+
